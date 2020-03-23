edca9c84d71c7e99e55318ac3eb72bd330262c13
AdrianaEG <adriana.ngc438@gmail.com>
2020-03-23 04:50:03 -0300

    Promesas

--- clase-12-jquery.js
+++ clase-12-jquery.js
@@ -87,7 +87,8 @@ miObjeto
     .decirChau()
     .decirHola();
 
-//cuando hago esto de arriba miObjeto.decirHola() mi interprete ejecuta el console.log(hola)y me devuelve el objeto de manera que le puedo decir que siga haciendo cosas
+//cuando hago esto de arriba miObjeto.decirHola() mi interprete ejecuta el console.log(hola)y me devuelve el objeto de manera que
+// le puedo decir que siga haciendo cosas
 //es decir que cada función devuelve el mismo objeto de manera tal que se puede ir concatenando.
 
 //PROMESAS. ¿Qué es?
@@ -95,7 +96,65 @@ miObjeto
 //https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Promise
 
 //Promesa: definición--> es que yo te estoy asegurando que algo va a pasar en el futuro
+//vamos a ver promesas porque lo usa cypress y porque es útil para el concepto de API
+//una promesa tiene dos estados: pendiente o terminada.
+//Pendiente es no sé el resultado, no sé si me han cumplido esa promesa.
+//una promesa pendiente tiene dos estados futuros, es decir pueden pasar dos cosas
+//puede que se cumpla o puede ser que no se cumpla esa promesa
 
+//en programación se usan promesas porque hay operaciones que tardan mucho tiempo por ej ir a buscar datos a un sitio web externos y no podemos bloquear
+//al usuario mientras se espera la respuesta del sitio web
+//entonces mientras se espera la respuesta se siguen haciendo/siguen pasando cosas.
+//y después se avisa que pasó. 
 
+//como se usa una promesa en la vida real: 
+//api publica que da el cambio de moneda al dia de hoy. 
+//https://api.exchangeratesapi.io/latest
+//me da el cambio de cada moneda respecto al euro. (estos sonn datos que vamos a buscar a un sitio web)
+//al ser datos que vamos a buscar de un sitio web, para buscar esos datos vamos a hacer un request, ese request puede fallar o tardar en responder
+//Si operamos de un modo sincrono, es decir... que si se quieren los datos hay que esperar y no se use la página hasta no tener una respuesta
+//para que no pase eso usamos fecth que devuelve una promesa 
+fetch("https://api.exchangeratesapi.io/latest")//esta promesa es un objeto que tiene un método then
+.then(respuesta => respuesta.json())//el métdo then significa hace esto y una vez que lo hagas ejecuta esta funcion, el then lo que tiene es que también se
+// puede concatenar(concepto de chaining se puede aplicar acá)
+//lo siguiente es equivalente a lo de arriba:
+//.then(function(respuesta){return respuesta.json();});
+.then(respuesta =>{//esta respuesta es el resultado de respuesta.json(). then siempre toma un parámetro, ese parámetro está definido por la función anterior
+    //$("#resultado").text($('#resultado').text() + JSON.stringify(respuesta));
+    console.log('el sitio web contesto');
+})
+.catch(error => console.error("falló", error));//el .catch es para decir... si no pudiste resolver que me myestre un error. 
 
+console.log("esto pasa antes que la respuesta de fetch");
 
+//esto es importante... antes para hacer lo mismo que hoy en dia logramos hacer con un fetch era larguísimo de hacer, entonces jquery envolvió todo con ajax 
+//que significa asynchronous jvascript and XML, 
+//antes para transferir datos se usaba XML
+//JSON:
+//{nombre: "Adriana", apellido: Gonzalez}
+//XML:
+//<persona><nombre>Adriana</nombre><apellido>Gonzalez</apellido></persona>
+//ajax es estar en la página web y mientras estoy en la página web hacer un request a un servidor y traer datos, los datos no solo pueden estar en xml tambien
+// pueden estar en json
+
+//en ajax se haría así: 
+/* ESTO ES AJAX HOY EN DIA SE USA MÁS FETCH
+
+$.ajax({
+    method: "GET",
+    url: "https://api.exchangeratesapi.io/latest",
+    //success: function (respuesta){}
+    success: respuesta => { //puedo consultar la documentacion jquery ajax. success es una función que va a ser llamada si el request es exitoso, lleva tres argumentos...
+        console.log("respuesta de exchangeratespi.io", respuesta);
+        $("#resultado").text(JSON.stringify(respuesta));
+    },
+    async: true
+    //async: false //al descomentar esta linea nada se ejecuta hasta que esta llamada termine.
+    //significa que no quiero que sea asíncrono. 
+    //es decir que si async es true... lo que va a suceder es que
+    // la página va a seguir funcionando mientras espera
+    //que se carguen los datos que viene del otro sitio web
+});
+//asíncrono significa que no va uno después del otro...
+console.log("esto pasa antes de la respuesta de $.ajax");
+*/
